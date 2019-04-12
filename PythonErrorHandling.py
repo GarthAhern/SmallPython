8675708a2baee0fcd356a0970972591e6d02780d
GarthAhern <aherng@outlook.com>
2019-04-11 18:23:46 -0700

    A more interesting version that only uses the compute() method.

--- PythonErrorHandling.py
+++ PythonErrorHandling.py
@@ -1,20 +1,26 @@
 def getInfo():
     var1 = input("\nPlease provide the first numeric value: ")
     var2 = input("\nPlease provide the second numeric value: ")
-    compute(var1, var2)
+    return var1, var2
 
 
 
-def compute(var1,var2):
-    try:
-        var3 = int(var1) + int(var2)
-        print("{} + {} = {}".format(var1,var2,var3))
-    except ValueError as e:
-        print("{}: \nYou did not provide a numeric value!".format(e))
-    except:
-        print("Program will close now.")
-    finally:
-        print("Moving on.......")
+def compute():
+    go = True
+    while go:
+        var1,var2 = getInfo()
+        try:
+            var3 = int(var1) + int(var2)
+            
+            go = False
+        except ValueError as e:
+            print("{}: \nYou did not provide a numeric value!".format(e))
+        except:
+            print("Program will close now.")
+    #finally:
+        #print("Moving on.......")
+    print("{} + {} = {}".format(var1,var2,var3))
+    
         
 
 
@@ -22,5 +28,5 @@ def compute(var1,var2):
 
 
 if __name__=="__main__":
-    getInfo()
+    compute()
     
