574f8316d581de0a1512059e82b96096e5b32917
GarthAhern <aherng@outlook.com>
2019-04-11 18:11:13 -0700

    a great example of exception handling

--- PythonErrorHandling.py
+++ PythonErrorHandling.py
@@ -9,10 +9,13 @@ def compute(var1,var2):
     try:
         var3 = int(var1) + int(var2)
         print("{} + {} = {}".format(var1,var2,var3))
-    except ValueError:
-        print("You did not provide a numeric value!")
+    except ValueError as e:
+        print("{}: \nYou did not provide a numeric value!".format(e))
     except:
         print("Program will close now.")
+    finally:
+        print("Moving on.......")
+        
 
 
 
